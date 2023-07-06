c021f5b3075bdd12491672a829ec9f4f53d66696
Arya Emami <aryaemami59@yahoo.com>
2023-07-06 17:08:59 -0500

    bumped version

--- index.ts
+++ index.ts
@@ -1,9 +1,13 @@
+// Utilities
+import jsonStringify from "./src/jsonStringify";
+
 // Arrays
 import arrayDifference from "./src/arrayDifference";
 import arrayIntersection from "./src/arrayIntersection";
 import arraySymmetricDifference from "./src/arraySymmetricDifference";
 
 // Objects
+import objectEntries from "./src/objectEntries";
 import objectKeys from "./src/objectKeys";
 
 // Strings
@@ -16,7 +20,9 @@ export {
   arrayIntersection,
   arraySymmetricDifference,
   capitalize,
+  jsonStringify,
   lowerCase,
+  objectEntries,
   objectKeys,
   upperCase,
 };
