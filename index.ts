07be948a303b3e9f75224f62ab7840944de69536
Arya Emami <aryaemami59@yahoo.com>
2023-08-09 15:05:49 -0500

    updated deps

--- index.ts
+++ index.ts
@@ -1,27 +1,34 @@
 // Utilities
 import fetchWrapper from "./src/browser/fetchWrapper";
+import jsonStringify from "./src/node/jsonStringify";
+import nodeFetchWrapper from "./src/node/nodeFetchWrapper";
+
 // Arrays
 import arrayDifference from "./src/node/arrayDifference";
 import arrayIntersection from "./src/node/arrayIntersection";
 import arraySymmetricDifference from "./src/node/arraySymmetricDifference";
-import jsonStringify from "./src/node/jsonStringify";
-import nodeFetchWrapper from "./src/node/nodeFetchWrapper";
+import countDuplicates from "./src/node/countDuplicates";
+
+// Objects
 import objectEntries from "./src/node/objectEntries";
 import objectKeys from "./src/node/objectKeys";
 import pick from "./src/node/pick";
-import split from "./src/node/split";
 
 // Strings
 import capitalize from "./src/node/capitalize";
 import toLowerCase from "./src/node/toLowerCase";
 import toUpperCase from "./src/node/toUpperCase";
+import split from "./src/node/split";
+import join from "./src/node/join";
 
 export {
   arrayDifference,
   arrayIntersection,
   arraySymmetricDifference,
   capitalize,
+  countDuplicates,
   fetchWrapper,
+  join,
   jsonStringify,
   nodeFetchWrapper,
   objectEntries,
--- src/node/split.ts
+++ src/node/split.ts
@@ -3,8 +3,6 @@ import type { Split } from "@aryaemami59/ts-util-types";
 const split = <const S extends string, const D extends string>(
   str: S,
   separator: D
-): Split<S, D> => {
-  return str.split(separator) as Split<S, D>;
-};
+): Split<S, D> => str.split(separator) as Split<S, D>;
 
 export default split;
