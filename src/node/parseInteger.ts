8c88f096a5d44560be304d3e41b038a9a1bff460
Arya Emami <aryaemami59@yahoo.com>
2023-08-25 14:25:39 -0500

    updated deps

--- src/node/parseInteger.ts
+++ src/node/parseInteger.ts
@@ -3,11 +3,9 @@ import type {
   StringToNumber,
 } from "@aryaemami59/ts-util-types";
 
-export const parseInteger = <const S extends `${number}`>(
+export const parseInteger = <const S extends `${bigint}`>(
   string: S,
   radix?: ExclusiveRange<37, 2>
 ): StringToNumber<S> => parseInt(string, radix) as StringToNumber<S>;
-
-const element = parseInteger("0.");
 
 export default parseInteger;
